a61f3d79f13c4307d526b5aa9b36037015c7dbd8
habeelety <habeelety@gmail.com>
2012-11-28 16:18:42 +0100

    Google Map By Habeelety

---
+++ map.php
@@ -0,0 +1,16 @@
+<?php
+
+$list = array (
+    'aaa,bbb,ccc,dddd',
+    '123,456,789',
+    '"aaa","bbb"'
+);
+
+$fp = fopen('file.csv', 'w');
+
+foreach ($list as $line) {
+    fputcsv($fp, split(',', $line));
+}
+
+fclose($fp);
+?> 
